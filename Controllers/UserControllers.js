b16cd4032ad247db34f57fb1dc4910f4b5123d23
3ibsi69 <a.abbassi21306@pi.tn>
2023-07-05 10:28:18 +0100

    added user controllers

---
+++ Controllers/UserControllers.js
@@ -0,0 +1,66 @@
+var User = require("../Modules/User");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+
+const signup = async (req, res) => {
+  var checkUser = await User.findOne({ email: req.body.email });
+  if (checkUser) {
+    res.send({ msg: "email already exist" });
+  }
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(req.body.password, salt, async (err, hash) => {
+      var user = {
+        email: req.body.email,
+        username: req.body.username,
+        password: hash,
+      };
+      var createdUser = await User.create(user);
+      var token = jwt.sign({ id: createdUser._id }, "c21");
+      res.send({ token });
+    });
+  });
+};
+
+const login = async (req, res) => {
+  var user = await User.findOne({ email: req.body.email });
+  if (user) {
+    bcrypt.compare(req.body.password, user.password, function (err, result) {
+      if (result) {
+        var token = jwt.sign({ id: user._id }, "c21");
+        res.send({ token });
+      } else {
+        res.send({ msg: "wrong password" });
+      }
+    });
+  } else {
+    res.send("wrong email");
+  }
+};
+
+const verify = async (req, res) => {
+  if (!req.body.token) {
+    res.send({ msg: false });
+  }
+  try {
+    var payload = jwt.verify(req.body.token, "c21");
+    if (payload) {
+      var user = await user.findOne({ _id: payload.id });
+      if (user) {
+        var token = jwt.sign({ id: user._id }, "c21");
+        res.send({ user });
+      } else {
+        res.send("invalid token");
+      }
+    } else {
+      res.send("invalid token");
+    }
+  } catch (err) {
+    res.send("invalid token");
+  }
+};
+
+module.exports = {
+  signup,
+  login,
+  verify,
+};
