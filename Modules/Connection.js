a38e744d1a32ba87344ac232a17a08c4f3bcfc74
3ibsi69 <a.abbassi21306@pi.tn>
2023-07-05 09:52:03 +0100

    changed name db

--- Modules/Connection.js
+++ Modules/Connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(
-    "mongodb+srv://abbassi2002ahmed4:Ahmed2002@cluster0.8eolprl.mongodb.net/"
+    "mongodb+srv://abbassi2002ahmed4:Ahmed2002@cluster0.8eolprl.mongodb.net/auth-todo-server"
   );
 }
 
