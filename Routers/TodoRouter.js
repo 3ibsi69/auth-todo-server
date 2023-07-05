94dffaa531942d51e72582f9433225ea13cc150c
3ibsi69 <a.abbassi21306@pi.tn>
2023-07-05 10:33:08 +0100

    added todo-router

---
+++ Routers/TodoRouter.js
@@ -0,0 +1,14 @@
+const router=require("express").Router();
+
+var todoController=require("../Controllers/TodoControllers");
+
+router.get("/",todoController.getAllTodo);
+router.post("/",todoController.postOneTodo);
+router.delete("/:id",todoController.deleteTodo);
+router.put("/:id",todoController.updateTodo);
+router.get("/:userId",todoController.getAllUSerTodo);
+
+
+
+
+module.exports=router;
